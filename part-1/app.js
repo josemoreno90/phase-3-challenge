857125cebb6e538175e8adfc33b3b676a8ad0ab5
Jose Moreno <morenojr90@yahoo.com>
2017-10-10 18:49:28 -0700

    Finish supported operators route

--- part-1/app.js
+++ part-1/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.get('/', function(req, res){
-  res.send("HomePage...")
+//1st Route (GET Request)
+//this route returns supported operators of content type application/json
+app.get('/api/supported-operators', function(req, res){
+  res.send({
+    "/": "division",
+    "+": "addition",
+    "-": "subtractionn",
+    "*": "multiplication"
+  })
 })
+
+
+
 
 app.listen(3000, function(){
   console.log("Listening NOW")
