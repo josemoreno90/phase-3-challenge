9d60715c9e68f64aa43d199bf17f36fe00ca2594
Jose Moreno <morenojr90@yahoo.com>
2017-10-12 14:34:39 -0700

    Finish database and database_test

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,15 +1,23 @@
+DROP DATABASE IF EXISTS grocery_store;
+CREATE DATABASE grocery_store;
+
+\c grocery_store;
+
+DROP TABLE IF EXISTS shoppers;
 CREATE TABLE shoppers (
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
 	id SERIAL PRIMARY KEY,
 	order_date TIMESTAMP DEFAULT NOW(),
 	shopper_id SERIAL REFERENCES shoppers(id)
 );
 
+DROP TABLE IF EXISTS items;
 CREATE TABLE items (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
@@ -17,6 +25,7 @@ CREATE TABLE items (
 	section VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS order_items;
 CREATE TABLE order_items (
 	order_id SERIAL REFERENCES orders(id),
 	item_id SERIAL REFERENCES items(id)
